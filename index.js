2c6796d21d4783aa7275b6cd4b5e7b17b103081b
estebandalel-p44 <76073555+estebandalel-p44@users.noreply.github.com>
2022-02-28 23:35:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-"use strict";
-const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
-const client = new SecretManagerServiceClient();
-
-exports.http = async (request, response) => {
-  // Access the secret.
-  const [accessResponse] = await client.accessSecretVersion({
-    name: "projects/781385493461/secrets/AIRTABLE_API_KEY/versions/latest",
-  });
-
-  const responsePayload = accessResponse.payload.data.toString("utf8");
-  if (responsePayload) {
-    console.info(`Payload: ${responsePayload}`);
-    response.status(200).send(`Payload: ${responsePayload}`);
-  } else response.status(501).send(responsePayload);
-};
-
-exports.event = (event, callback) => {
-  callback();
-};
-
-// en un momento del tiempo unixtime, mandar preguntas al canal en el installation token
-// 24 hs después correr create
-// una semana depués flushear rooms
+// This code sample uses the 'node-fetch' library:
+// https://www.npmjs.com/package/node-fetch
+import fetch from "node-fetch";
+// this will get this template https://project44.atlassian.net/wiki/spaces/PMO/pages/35540500483/YYYY-MM-DD-title
+// it's the official template for PMAs
+fetch(
+  "https://project44.atlassian.net/wiki/rest/api/content/35540500483?expand=body.storage,space,container,ancestors",
+  {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Basic ZXN0ZWJhbi5kYY1Qw==",
+      Accept: "application/json",
+    },
+  }
+  // the authorization token is a base64 encoding of <yourmail>:<yourAPIkey>
+  // in my case: esteban.dalel@project44.com:9QbdZjTA5YnFhPU465C
+  //OBVIOUSLY OBFUSCATED by deleting a few chars lol
+  // do it with https://www.base64decode.org and https://www.base64encode.org
+  // to get you api key https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/
+)
+  .then((response) => response.json())
+  .then((data) => {
+    // you should dinamically change the title, maybe with dateISO or something
+    let bodyData = {
+      title: "testAPI7",
+      type: "page",
+      space: data.space,
+      container: data.container,
+      body: data.body,
+      ancestors: data.ancestors,
+    };
+    console.log(data);
+    console.log(bodyData);
+    //now post all the content we just fetched...
+    fetch("https://project44.atlassian.net/wiki/rest/api/content", {
+      method: "POST",
+      headers: {
+        Authorization:
+          "Basic ZXN0ZWJhbimRaalRBU9WNDY1Qw==",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyData),
+    })
+      .then((response) => {
+        console.log(`Response: ${response.status} ${response.statusText}`);
+        return response.json();
+      })
+      // this will log the url for the created page
+      .then((text) => console.log(text._links.self))
+      .catch((err) => console.error(err));
+  })
+  .catch((err) => console.error(err));
